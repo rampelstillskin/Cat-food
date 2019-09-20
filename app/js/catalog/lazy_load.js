ab6be751c0c37aa4a3bea5d507db6b33fa4541a1
Andrey Kharitonov <rampelstillskin@gmail.com>
2019-09-20 16:20:46 +0300

    добавил кнопку 'показать еще'

---
+++ app/js/catalog/lazy_load.js
@@ -0,0 +1,10 @@
+$(function() {
+	$(".catalog__item").slice(0, 6).css("display", "flex");
+	$(".catalog__item-show__button").on("click", function(evt){
+		evt.preventDefault();
+		$(".catalog__item:hidden").slice(0, 3).css("display", "flex");
+		if ($(".catalog__item:hidden").length === 0) {
+			$(".catalog__item-show").css("display", "none");
+		}
+	});
+});
